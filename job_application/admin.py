b03ddd33f868b3da5d5cea8cacd01091a7431079
Ronaldo <dobreticr233@hotmail.com>
2025-03-28 12:57:30 +0100

    Adding Navigation bar and about page

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email") # Prikazuje sve navedeno bez da se stisne na ime
+    search_fields = ("first_name", "last_name", "email")  # Dodaje search opciju za brzinsko pretrazivanje
+    list_filter = ("date", "occupation")  #Filtriraj ovisno o datumu ili zaposlenju
+    ordering = ("first_name", ) # Organizira se po prvom slovu imena
+    readonly_fields = ("occupation", ) #Samo se moze procitati nemoze se promjeniti
+
+admin.site.register(Form, FormAdmin)
